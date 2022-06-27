1be4bcc088763cdc1c39fe8e3543c9f7cef9c86c
Alexandra <a.r.chumachenko@gmail.com>
2022-06-27 15:22:12 +0300

    Init git

---
+++ JsonSerialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
