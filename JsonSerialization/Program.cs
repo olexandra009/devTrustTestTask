8b106fa64500f9588596dd0286734d160809e573
Alexandra <a.r.chumachenko@gmail.com>
2022-06-28 20:52:39 +0300

    Implementing deserialization for Person in SimpleParser

--- JsonSerialization/Program.cs
+++ JsonSerialization/Program.cs
@@ -9,8 +9,10 @@
             a.ToObject<Address>();
 
             var pa = scratch.ParseToDictionary("{firstName: ‘Ivan’, lastName: ‘Petrov’, address: {city: ‘Kiev’, addressLine: prospect “Peremogy” 28/7,}", typeof(Person));
-            pa.ToObject<Person>();
-         
+            //pa.ToObject<Person>();
+
+            SimpleJsonParser parser = new SimpleJsonParser();
+            var pe = parser.Deserialize("{firstName: ‘Ivan’, lastName: ‘Petrov’, address: {city: ‘Kiev’, addressLine: prospect “Peremogy” 28/7,}");
 
         }
 
