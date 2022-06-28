a34d67d29fb1c9947bb774649eac2c536245c109
Alexandra <a.r.chumachenko@gmail.com>
2022-06-28 21:04:08 +0300

    Add template for api

---
+++ TestTaskApi/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace TestTaskApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpPost]
+        public Task<long> Save(string json)
+        {
+            // deserialize string into object of type Person using own deserializer (see more details in Minimal requirements section)
+            // DO NOT use 3rd party libraries for deserialization like Json.NET or Microsoft
+            // insert or update Person entity in database
+            // return entity id
+            throw new NotImplementedException();
+        }
+        [HttpGet]
+        public Task<string> GetAll(GetAllRequest request)
+        {
+            // get Persons entities from database
+            // filter by GetAllRequest fields (null or empty fields should be ignored)
+            // use your own manually written json serializer to serialize result into string
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
