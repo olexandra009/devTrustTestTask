0a1caf8dac97469032360b9c81650d931d6a08fb
Alexandra <a.r.chumachenko@gmail.com>
2022-06-29 12:14:49 +0300

    Add autofac

---
+++ TestTaskApi/AutofacModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using TestTaskApi.Data.Repository;
+
+namespace TestTaskApi
+{
+    public class AutofacModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<PersonRepository>()
+                .As<IPersonRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<AddressRepository>()
+                .As<IAddressRepository>().InstancePerLifetimeScope();
+
+            builder.RegisterType<SimpleJsonParser>()
+                .As<ISimpleJsonParser>().InstancePerLifetimeScope();
+        }
+    }
+}
